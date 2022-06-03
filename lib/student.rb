11d6391d673e9ffdea4bfac8a898fd9ca1939cd0
Sungwoo Kim <sungwoo.kim@yale.edu>
2022-06-03 10:41:09 -0700

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+require_relative "../lib/user.rb"
 
+class Student < User
+    
+    attr_accessor :knowledge
+#Individual students should initialize with an instance variable, @knowledge, that points to an empty array.
+    def initialize
+        @knowledge = []
+    end
+#Define a method, #learn, that takes in a string and adds that string to the student's @knowledge array.
+   def learn(knowledge)
+        @knowledge << knowledge
+   end
+#Define a method, #knowledge, that returns that student's knowledge array.
+   def knowledge
+    @knowledge
+   end
 end
